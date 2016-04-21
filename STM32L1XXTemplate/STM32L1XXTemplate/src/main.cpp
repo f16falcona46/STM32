286c5eec33d3892345fc31962de12e5a5138219d
Jason Li <jcl01010011@gmail.com>
2016-04-21 11:57:31 -0400

    Committed template

---
+++ STM32L1XXTemplate/STM32L1XXTemplate/src/main.cpp
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32l1xx.h"
+#include "stm32l100c_discovery.h"
+
+int main() {
+	while(1);
+}
